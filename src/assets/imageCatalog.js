51a4030712da101443e3345f4d9bc7edeafa8adc
Wilfred0zz <wilfredo.recinos52@myhunter.cuny.edu>
2021-05-20 16:29:11 -0400

    added images for seeds

--- src/assets/imageCatalog.js
+++ src/assets/imageCatalog.js
@@ -4,12 +4,22 @@ const imageCatalog = {
   1 : 'pot.svg',
   2 : 'watering-can.svg',
   3 : 'fertilizer.svg',
-  4 : 'seeds.svg',
+  4 : 'cactus.svg',
   5 : 'seeds.svg',
-  6 : 'seeds.svg',
-  7 : 'seeds.svg',
-  8 : 'seeds.svg',
-  9 : 'seeds.svg',
-  10 : 'seeds.svg'
+  6 : 'grass.svg',
+  7 : 'aloe.svg',
+  8 : 'adenium.svg',
+  9 : 'rose.svg',
+  10: 'marijuana.svg',
+  11: 'tulip.png',
+  12: 'caladium.png',
+  13: 'seeds.svg',
+  14: 'lily.svg',
+  15: 'iris.svg',
+  16: 'marigold.png',
+  17: 'lavender.png',
+  18: 'magnolia.png',
+  19: 'plant.png',
+  20: 'seeds.svg'
 }
 export default imageCatalog;
